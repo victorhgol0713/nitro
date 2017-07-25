0c3944a0de3fb0e47fa3a4f716b24c141106db1f
marrquez <marrquez@hotmail.com>
2017-07-25 17:03:40 -0500

    configuring webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.less', '.css']
   },
 
   module: {
@@ -34,21 +34,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
         loader: 'file-loader?name=assets/[name].[hash].[ext]'
       },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        loader: 'raw-loader!less-loader'
-      }
+      { test: /.less$/, exclude: /node_modules/, loader: 'raw-loader!less-loader' }
     ]
   },
 
