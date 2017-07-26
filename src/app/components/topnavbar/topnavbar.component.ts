750e26ba5e2eb2b61bb28a3e8fa8e429c98ca8d4
marrquez <marrquez@hotmail.com>
2017-07-26 17:27:40 -0500

    adding stylesheet to the navbar

--- src/app/components/topnavbar/topnavbar.component.ts
+++ src/app/components/topnavbar/topnavbar.component.ts
@@ -4,12 +4,13 @@ declare var jQuery: any;
 
 @Component({
   selector: 'topnavbar',
-  templateUrl: './topnavbar.template.html'
+  templateUrl: './topnavbar.template.html',
+  styles: [ require('./topnavbar.component.less') ]
 })
 export class TopNavbarComponent {
-
   toggleNavigation(): void {
     jQuery("body").toggleClass("mini-navbar");
+    jQuery(".chevron").toggleClass("chevron-right");
     smoothlyMenu();
   }
 }
