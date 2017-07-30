b28c8a5c2de8a064680a9f7e6b8acbd0444f8660
Cristian Olaya <cristianolaya@MacBook-Pro-de-Cristian.local>
2017-07-29 22:07:10 -0500

    enabling the main carrousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,54 @@
 import { Component, OnDestroy, OnInit, } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
   selector: 'home',
-  templateUrl: './home.template.html'
+  templateUrl: './home.template.html',
+  styles: [ require('./home.component.less') ]
 })
 export class HomeComponent implements OnDestroy, OnInit  {
+  public nav: any;
+  public constructor() {
+    this.nav = document.querySelector('nav.navbar');
+  };
 
-public nav:any;
+  public ngOnInit(): any {
+    this.nav.className += " white-bg";
 
-public constructor() {
-  this.nav = document.querySelector('nav.navbar');
-}
+    jQuery('.slick_demo_2').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  };
 
-public ngOnInit():any {
-  this.nav.className += " white-bg";
-}
-
-
-public ngOnDestroy():any {
-  this.nav.classList.remove("white-bg");
-}
-
+  public ngOnDestroy(): any {
+    this.nav.classList.remove("white-bg");
+  };
 }
